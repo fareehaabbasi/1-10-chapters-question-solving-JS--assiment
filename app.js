8b51e4e48b2f54d8d1dec688faba3ec3e0a16c18
fareehaabbasi <abbasifareeha9@gmail.com>
2025-03-12 02:35:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,171 @@
+//chapter#1 --------------------Alerts.
+// alert ('Hello to js land.....')
+// alert("Happy Coding");
+// alert("Hello I can run js through my web browser's console");
+
+//---------------------------------END------------------------------
+
+
+
+
+//chapter#2 -------------------Variables For Strings.
+//1;-
+// var userName = 'Ali';
+// alert (userName);
+
+// //2
+// var fullName = "Fareeha Abbasi";
+// alert(fullName);
+
+// //3
+// let message = "Hello World!";
+// alert(message);
+
+// //4 
+// let Name = "John";
+// let age = 16;
+// let work = "Certificate Mobile applicant Development";
+// alert(Name);
+// alert(age);
+// alert(work);
+
+// //5
+// let food = ["pizza", "burger", "sandwich"];
+// alert(food);
+
+//6
+// let email = "abbasi@.com";
+// alert("This is my email address " + "" + email);
+
+//7 
+// let book = "A smarter way to learn javascript";
+// alert("I am trying to learn from the Book " + book);
+
+//8 
+// let display = "Yah! I can write HTML content through Javascript";
+// document.write(display);
+
+//9
+// let food = ["pizza", "burger", "sandwich"];
+// alert(food);
+// document.write (food);
+
+//---------------------------------END------------------------------
+
+
+
+
+//Chapter#3 ------------- Variables For Number.
+//1
+// let age = 19;
+// alert("I am " + age + " years old.");
+
+//2 
+// let visit = 14;
+// alert("You have visit this website " + visit + " times");
+
+//3
+// let birthday = 2005;
+// alert("My birth year is " + birthday);
+// document.write("My birth year is " + birthday);
+
+//4
+// let visitorName = "Minhan abeer";
+// let productTitle = "M-size-Maxi";
+// let quantity = 5;
+// document.write(visitorName + " ordered " + productTitle + " total quantity is " + quantity + " in XYZ store");
+
+//----------------------------------END---------------------------------
+
+
+
+
+//Chapter#4 ----------------Variables Names: Legal & Illegal
+//1
+// let x = 10, y = 20, z = 30;
+// document.write(x , y ,z);
+
+//2
+// ------------- 5 Legal Variable Names
+// let firstName;     //Valid: Starts with a letter, uses camelCase.
+// let _count;        //Valid: Starts with an underscore.
+// let totalAmount$;  //Valid: Contains a dollar sign (allowed in variable names).
+// let user2;         //Valid: Contains a number, but doesn't start with one.
+// let myVar;         //Valid: Simple and follows naming conventions.
+// -------------- 5 Illegal Variable Names
+// let 1stPlace;      //Invalid: Starts with a number.
+// let my-var;        //Invalid: Contains a hyphen (-), which is not allowed.
+// let var;           //Invalid: var is a reserved keyword in JavaScript.
+// let my var;        //Invalid: Contains a space.
+// let @email;        //Invalid: Starts with a special character (@), which is not allowed.
+
+//3
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<h4>Variables names can only contained $, numbers, _, letter, Or Capitalized</h4>");
+// document.write("<h4>Variables must begins with a letter, $, or Capitalized</h4>");
+// document.write("<h4>Variable names are Case Sensitive</h4>");
+// document.write("<h4>Variable should not be JS keywords</h4>");
+
+//----------------------------------END---------------------------------
+
+
+
+
+//Chapter#5 --------------- Math Expression
+// 1
+// let num1 = 6;
+// let num2 = 10;
+// let num = (6 + 10);
+// document.write("Sum of " + num1 + " and " + num2 + " is " + num);
+
+//2
+// let num1 = 6;
+// let num2 = 10;
+// let num = (6 - 10);
+// document.write("Subtraction of " + num1 + " and " + num2 + " is " + num);
+
+// let num1 = 6;
+// let num2 = 10;
+// let num = (6 * 10);
+// document.write("Multiplication of " + num1 + " and " + num2 + " is " + num);
+
+// let num1 = 6;
+// let num2 = 10;
+// let num = (6 % 10);
+// document.write("Modulus of " + num1 + " and " + num2 + " is " + num);
+
+// let num1 = 6;
+// let num2 = 10;
+// let num = (6 / 10);
+// document.write("Division of " + num1 + " and " + num2 + " is " + num);
+
+//3
+// let decl;
+// document.write("<ul><li>Value after declaration is: " + decl + "</li></ul>");
+// decl = 5;
+// document.write("<ul><li>Initialized value is: " + decl + "</li></ul>");
+// let decl1 = ++decl;
+// document.write("<ul><li>Value after increment is: " + decl1 + "</li></ul>");
+// let decl2 = decl1 + 7;
+// document.write("<ul><li>Value after addition is: " + decl2 + "</li></ul>");
+// let decl3 = --decl2;
+// document.write("<ul><li>Value after decrement is: " + decl3 + "</li></ul>");
+// let rem = decl3 % 3;
+// document.write("<ul><li>The Reminder is: " + rem + "</li></ul>");
+
+//4
+//Buying-ticket cost
+// let price = 600;
+// let calc = price * 5;
+
+// document.write("Total cost to buy 5 tickets is " + calc);
+
+//5 
+
+
+
+
+
+
+
+
